4c2882a2cd257fafcb35c693fd549f07b8e4cae2
Russell Christensen <russellchristensen@me.com>
2016-11-04 14:46:38 +0000

    forecast with zipcode working

---
+++ forecast/forecast.rb
@@ -0,0 +1,27 @@
+require 'httparty'
+require 'json'
+require 'geocoder'
+
+api_key = "361f798bbccc4637e336d8ba047ae3eb"
+
+
+puts "What zipcode would you like to know the Forecast for?"
+zipcode = gets
+
+lat = Geocoder.search(zipcode)[0].geometry['location']['lat']
+lng = Geocoder.search(zipcode)[0].geometry['location']['lng']
+
+url = "https://api.darksky.net/forecast/#{api_key}/#{lat},#{lng}"
+
+
+#response = HTTParty.get(url, {format: :json})
+response = HTTParty.get(url)
+#puts "Latitude: #{lat} \t Longitude: #{lng}"
+
+#puts response.body[:currently]
+
+
+symbol = JSON.parse(response.body, symbolize_names: true)
+temp = symbol.dig :currently, :temperature
+summary = symbol.dig :minutely, :summary
+puts summary + " It's currently #{temp} degrees"
